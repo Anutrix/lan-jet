aacd71b58f27567be3d10aebefb939328c20fcbd
Anutrix <numaanzaheerahmed@yahoo.com>
2024-05-02 03:20:02 +0530

    Refactored, made it work in Android and improved self and peer IP detection logic

--- Autoloads/NetworkUtils.gd
+++ Autoloads/NetworkUtils.gd
@@ -1,31 +1,77 @@
 extends Node
 
-func _ready() -> void:
-	pass
+var http_requests_holder: Node = null
+
+const LANJET_VERIFICATION_STRING: String = "Lan-Jet is alive!"
+const PEER_REFRESH_TIME_SECONDS: float = 4.0
+
+var self_ips: PackedStringArray = []
+var peer_ip_states: Dictionary = {}
 
 func _process(_delta: float) -> void:
 	pass
 
-func getInterfaceIPv4Addresses() -> Array[String]:
-	var ipList: Array[String] = []
-	var os_name: String = OS.get_name()
+func _ready() -> void:
+	repopulate_self_IPs()
 	
-	# TODO: Add support for other OSes
-	if os_name == "Windows" or os_name == "UWP":
-		var output: Array[String] = []
-		var exit_code: int = OS.execute("cmd.exe", ["/C", "ipconfig"], output)
-		if exit_code == 0:
-			if len(output) == 1:
-				var lines: PackedStringArray = output[0].split("\r\n")
-				for line: String in lines:
-					if "IPv4 Address" in line:
-						var ip: String = line.split(":")[-1].strip_edges()
-						ipList.append(ip)
-			else:
-				print("Unexpected response to 'ipconfig' command:", output)
+	http_requests_holder = Node.new()
+	http_requests_holder.name = "HTTPRequestHolder"
+	add_child(http_requests_holder)
+	
+	update_peer_address_states()
+	create_peer_refresh_timer()
+
+func repopulate_self_IPs() -> void:
+	self_ips = IP.get_local_addresses()
+
+func create_peer_refresh_timer() -> void:
+	var timer: Timer = Timer.new()
+	timer.autostart = true
+	timer.one_shot = false
+	timer.wait_time = PEER_REFRESH_TIME_SECONDS
+	var err: int = timer.timeout.connect(self._on_timer_timeout)
+	if err != OK:
+		print("Timer signal timeout connection failed.")
+	add_child(timer)
+
+func _on_timer_timeout() -> void:
+	update_peer_address_states()
+
+func update_peer_address_states() -> void:
+	for http_req: HTTPRequest in http_requests_holder.get_children():
+		http_req.queue_free()
+	
+	for i: int in range(255):
+		var ip: String = "192.168.1." + str(i)
+		# Exclude own ips
+		if ip not in self_ips:
+			make_get_request(ip)
 		else:
-			print("Error: Couldn't run ipconfig")
-	else:
-		print("Unsupported OS")
+			peer_ip_states[ip] = false
+
+func make_get_request(ip: String) -> void:
+	var http_request: HTTPRequest = HTTPRequest.new()
+	#http_request.use_threads = true
+	http_request.timeout = 2.0
+	var connect_err: int = http_request.request_completed.connect(self._http_request_completed.bind(ip, http_request))
+	if connect_err != OK:
+		print("An error occurred in the request_completed signal connection.")
+		peer_ip_states[ip] = false
 	
-	return ipList
+	http_requests_holder.add_child(http_request)
+	
+	# Perform a GET request.
+	var error: Error = http_request.request("http://" + ip + ":8080")
+	if error != OK:
+		print("An error occurred in the HTTP request for IP: ", ip)
+		peer_ip_states[ip] = false
+
+func _http_request_completed(_result: int, _response_code: int, _headers: PackedStringArray, body: PackedByteArray, request_ip: String, http_request: HTTPRequest) -> void:
+	http_request.queue_free()
+	var resp: String = body.get_string_from_utf8()
+	
+	# Exclude unknown servers
+	if resp == LANJET_VERIFICATION_STRING:
+		peer_ip_states[request_ip] = true
+	else:
+		peer_ip_states[request_ip] = false
---
+++ PeerIPPanel/PeerIPPanel.gd
@@ -0,0 +1,41 @@
+extends Panel
+
+@onready var peer_ip_holder_node: Node = %PeerIPHolder
+
+const LABEL_REFRESH_TIME_SECONDS: float = 2.0 # Needs to be less than PEER_REFRESH_TIME_SECONDS
+
+func _ready() -> void:
+	update_peer_ip_panel()
+	create_peer_refresh_timer()
+
+func update_peer_ip_panel() -> void:
+	var current_ip_list: Array[String] = []
+	var latest_peer_ip_states: Dictionary = NetworkUtils.peer_ip_states
+	
+	# Remove lost connections
+	for child: Label in peer_ip_holder_node.get_children():
+		var ip: String = child.text
+		current_ip_list.append(ip)
+		if latest_peer_ip_states[ip] == false:
+			peer_ip_holder_node.remove_child(child)
+			child.queue_free()
+	
+	# Add new connections
+	for ip: String in latest_peer_ip_states:
+		if latest_peer_ip_states[ip] == true and ip not in current_ip_list:
+			var label: Label = Label.new()
+			label.text = ip
+			peer_ip_holder_node.add_child(label)
+
+func create_peer_refresh_timer() -> void:
+	var timer: Timer = Timer.new()
+	timer.autostart = true
+	timer.one_shot = false
+	timer.wait_time = LABEL_REFRESH_TIME_SECONDS
+	var err: int = timer.timeout.connect(self._on_timer_timeout)
+	if err != OK:
+		print("Timer signal timeout connection failed.")
+	add_child(timer)
+
+func _on_timer_timeout() -> void:
+	update_peer_ip_panel()
